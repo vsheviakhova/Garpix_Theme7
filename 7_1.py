f6941b5541f5d948cebe256b240b9e7b557f755e
vsheviakhova <vsheviakhova@gmail.com>
2022-06-15 11:56:58 +0300

    Initial commit

---
+++ 7_1.py
@@ -0,0 +1,8 @@
+def read_poem(path):
+    with open(path) as file:
+        counter = 0
+        while counter < 6:
+            print(file.readline().rstrip())
+            counter += 1
+
+read_poem('polozkova.txt')
